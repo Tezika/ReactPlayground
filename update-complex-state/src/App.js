ac232d341ffd2873137448a8434e1a18beb5741a
Tezika Zhou <tezikazhou@gmail.com>
2018-06-14 07:39:57 +0000

    refactor the 'IntructorItem' component to a stateless component

--- update-complex-state/src/App.js
+++ update-complex-state/src/App.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 
-class InstructorItem extends Component {
-    static propTypes = {
-      key: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      hobbies: PropTypes.arrayOf(PropTypes.string)
-    }
-    
-    render(){
-      return (
-        <li key={this.props.key}>
-        <h3>{this.props.name}</h3>
-        <h4>Hobbies: {this.props.hobbies.join(",")}</h4>
+//Stateless component(It doesn't have own state)
+const InstructorItem = props => {
+  return (
+    <li key={props.key}>
+        <h3>{props.name}</h3>
+        <h4>Hobbies: {props.hobbies.join(",")}</h4>
       </li>
-      );
-    }
-}
+  );
+};
+
+InstructorItem.propTypes = {
+    key: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    obbies: PropTypes.arrayOf(PropTypes.string)
+};
+
 
 class App extends Component {
   constructor(props) {
@@ -56,7 +56,7 @@ class App extends Component {
   render() {
     const instructors = this.state.instructors.map((instructor, index) => {
       return (
-         <InstructorItem 
+        <InstructorItem 
             key={index}
             name={instructor.name}
             hobbies={instructor.hobbies}
@@ -78,21 +78,21 @@ class App extends Component {
       Math.random() *
       this.state.instructors.length
     );
-    
+
     const hobbyIndex = Math.floor(
       Math.random() *
       this.state.instructors[instIndex].length
     )
-    
+
     const copyInsts = this.state.instructors.map((inst, i) => {
       if (i === instIndex) {
         const hobbies = [...inst.hobbies];
         hobbies.splice(hobbyIndex, 1);
-        return {...inst, hobbies };
+        return { ...inst, hobbies };
       }
       return inst;
     });
-    
+
     this.setState({
       instructors: copyInsts
     });
